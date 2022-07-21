8498769a6eba9fab3f29364f5d0572a30a0c2836
mykhaylo <pawlo.artymysyn@gmail.com>
2022-07-21 15:42:51 +0300

    change1

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,18 @@
 var http = require('http');
+var dt = require('./myfirstmodule');
+var url = require('url');
+var http = require('http');
+var fs = require('fs');
+
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!+!+!');
+ // res.writeHead(200, {'Content-Type': 'text/html'});
+  //res.write("The date and time are currently: " + dt.myDateTime() + "<br/>");
+  //var q = url.parse(req.url, true).query;
+  //var txt = q.year + " " + q.month;
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+  });
+  //res.end(txt);
 }).listen(8080); 
